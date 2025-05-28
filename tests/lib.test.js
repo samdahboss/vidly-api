773124076b9f68a1fc9033408f931c7f9ac26a0c
samdahboss <samuelolawumi65@gmail.com>
2025-05-28 08:26:07 +0100

    feat:  add tests for getProduct and registerUser functions

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -21,10 +21,38 @@ describe("greet", () => {
 });
 
 describe("getCurrency", () => {
-  const supportedCurrencies = ["AUD", "USD",  "EUR"];
+  const supportedCurrencies = ["AUD", "USD", "EUR"];
   it("should return an array of the supported currencies", () => {
     expect(lib.getCurrencies()).toEqual(
       expect.arrayContaining(supportedCurrencies)
     );
+  });
+});
+
+describe("getProduct", () => {
+  it("should return product with the given id", () => {
+    const product = lib.getProduct(1);
+    expect(product).toMatchObject({
+      id: 1,
+      price: 10,
+    });
+  });
+});
+
+describe("registerUser", () => {
+  it("should throw an error if the username is falsy", () => {
+    const args = [null, undefined, NaN, 0, "", false];
+    args.forEach((a) =>
+      expect(() => {
+        lib.registerUser(a);
+      }).toThrow()
+    );
+  });
+
+  it("should return a user object if valid username is provided", () => {
+    expect(lib.registerUser("username")).toMatchObject({
+      username: "username",
+      id: new Date().getTime(),
+    });
   });
 });
