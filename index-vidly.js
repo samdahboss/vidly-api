72064b802440d8f982c735b802462721db88bf96
samdahboss <samuelolawumi65@gmail.com>
2025-06-01 00:13:59 +0100

    fix: use environment variable for server port configuration

--- index-vidly.js
+++ index-vidly.js
@@ -12,7 +12,7 @@ routes(app);
 connectDB();
 checkConfig();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`App is running on port ${PORT}`);
 });
